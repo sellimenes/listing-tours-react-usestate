10271cfd07e3b7a91e6586fe0bba51f9bc53fbfd
sellimenes <sellim81@gmail.com>
2022-06-24 23:28:10 +0300

    some API data fetched

--- src/App.js
+++ src/App.js
@@ -10,7 +10,21 @@ function App() {
 
   const fetchTours = async () => {
     setLoading(true);
+
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
   };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
 
   if (loading) {
     return (
@@ -22,7 +36,7 @@ function App() {
 
   return (
     <main>
-      <Tours />
+      <Tours tours={tours} />
     </main>
   );
 }
