7e6e2e697f97007f8e1120fa31657d16456f33c5
sellimenes <sellim81@gmail.com>
2022-06-25 09:03:59 +0300

    displaying tours

--- src/Tours.js
+++ src/Tours.js
@@ -7,6 +7,11 @@ const Tours = ({ tours }) => {
         <h2>Our Tours</h2>
         <div className="underline"></div>
       </div>
+      <div>
+        {tours.map((tour) => {
+          return <Tour key={tour.id} {...tour} />;
+        })}
+      </div>
     </section>
   );
 };
