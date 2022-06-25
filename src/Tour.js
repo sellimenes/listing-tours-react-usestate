9a322d5586b7bd9c4aa468d50edec2a6a5ca5310
sellimenes <sellim81@gmail.com>
2022-06-25 09:50:14 +0300

    removing tours by id added

--- src/Tour.js
+++ src/Tour.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const Tour = ({ id, image, info, price, name }) => {
+const Tour = ({ id, image, info, price, name, removeTour }) => {
   const [readMore, setReadMore] = useState(false);
   return (
     <article className="single-tour">
@@ -13,10 +13,12 @@ const Tour = ({ id, image, info, price, name }) => {
         <p>
           {readMore ? info : `${info.substring(0, 200)}...`}
           <button onClick={() => setReadMore(!readMore)}>
-            {readMore ? "Read Less" : "Read More"}
+            {readMore ? "Show Less" : "Read More"}
           </button>
         </p>
-        <button className="delete-btn">not interested</button>
+        <button className="delete-btn" onClick={() => removeTour(id)}>
+          not interested
+        </button>
       </footer>
     </article>
   );
